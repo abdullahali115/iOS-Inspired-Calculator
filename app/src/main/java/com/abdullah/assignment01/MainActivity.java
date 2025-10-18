c9602008a4588977e280185dcfc6674097ad0afb
Abdullah Ali <abdullahpasari123@gmail.com>
2025-10-18 19:27:33 +0500

    Designed Landscape View

--- app/src/main/java/com/abdullah/assignment01/MainActivity.java
+++ app/src/main/java/com/abdullah/assignment01/MainActivity.java
@@ -1,8 +1,15 @@
 package com.abdullah.assignment01;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -10,15 +17,35 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageButton menuButton;
+    ImageButton menuButton2;
+    @RequiresApi(api = Build.VERSION_CODES.R)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        menuButton = findViewById(R.id.menuButton);
+        menuButton2 = findViewById(R.id.menuButton2);
+
+        menuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu obj = new PopupMenu(MainActivity.this, menuButton);
+                obj.getMenuInflater().inflate(R.menu.menu_items, obj.getMenu());
+                obj.show();
+            }
         });
+
+        menuButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu obj = new PopupMenu(MainActivity.this, menuButton2);
+                obj.getMenuInflater().inflate(R.menu.menu_items, obj.getMenu());
+                obj.show();
+            }
+        });
+
     }
 }
